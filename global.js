79ecee3b349bb02094dd9836faf323506326d250
julienz5 <julien.zana5@gmail.com>
2016-01-21 13:14:00 +0100

    a

--- global.js
+++ global.js
@@ -1,37 +1,45 @@
 // Custom JS
+
 $(document).ready(function(){
-	
+	console.log("wtf");
   // Add your code here
 
   //.........................................................................Smooth Scroll
-   	$(document).ready(function() {
+
  	$('.scrollTo').click( function() { // Au clic sur un élément
  	var page = $(this).attr('href'); // Page cible
  	var speed = 750; // Durée de l'animation (en ms)
  	$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
  	return false;
- 	});
- 
+ 	}); 
+
+
+ //.........................................................................Changer texte traduction
+
+
+$("#traduction").on('click',function (){
+  console.log("here i am");
+  $(this).text("Click to translate me");
+
 });
-   });
 
  //.........................................................................Back to Top
 
-if ($('#back-to-top').length) {
+if ($('#backtotop').length) {
     var scrollTrigger = 100, // px
         backToTop = function () {
             var scrollTop = $(window).scrollTop();
             if (scrollTop > scrollTrigger) {
-                $('#back-to-top').addClass('show');
+                $('#backtotop').addClass('show');
             } else {
-                $('#back-to-top').removeClass('show');
+                $('#backtotop').removeClass('show');
             }
         };
     backToTop();
     $(window).on('scroll', function () {
         backToTop();
     });
-    $('#back-to-top').on('click', function (e) {
+    $('#backtotop').on('click', function (e) {
         e.preventDefault();
         $('html,body').animate({
             scrollTop: 0
@@ -40,4 +48,4 @@ if ($('#back-to-top').length) {
 }
 
 
-
+   });
